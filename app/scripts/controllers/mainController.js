4bf9a94b4eccbf2d9ad6715f7ec539776383a32c
darlanmendonca <darlanmendonca@gmail.com>
2015-08-03 16:06:45 -0300

    init files

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name AppTest.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('AppTest')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name AppTest.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('AppTest')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
